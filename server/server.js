fe273dd5f9e972b9ce2faf9f6b356e29616eb559
Pattarawan Sriprasert <pattarawan@Macintosh.local>
2017-07-23 13:36:20 +0700

    Add server.test.js for test POST /todos route

--- server/server.js
+++ server/server.js
@@ -16,9 +16,9 @@ app.post('/todos', (req, res) => {
   });
   todo.save().then((doc) => {
     res.send(doc);
-   }, (e) => {
-     res.status(400).send(e);
-    });
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
 
@@ -26,3 +26,7 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Started on port 3000');
 })
+
+module.exports = {
+   app
+}
