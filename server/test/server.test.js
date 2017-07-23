308587730ff72b8355eff417c4ffacdd41d21c7a
Pattarawan Sriprasert <pattarawan@Macintosh.local>
2017-07-23 16:36:00 +0700

    Adding test for DELETE /todos/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -98,3 +98,37 @@ describe('GET /todos/:id', () => {
             .end(done);
     });
 });
+
+describe('DELETE /todos/:id', () => {
+    it('Should remove a todo', (done) => {
+        var hexId = todos[1]._id.toHexString();
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[1].text);
+            })
+            .end((err, res)=>{
+                if(err){
+                    return done(err);
+                }
+                Todo.findById(hexId).then((todo)=>{
+                    expect(todo).toNotExist();
+                    done();
+                }).catch((e)=> done(e));
+            });
+    })
+    it('Should return 404 if todo not found', (done) => {
+        var hexId = new ObjectID().toHexString();
+        request(app)
+            .get(`/todos/${hexId}`)
+            .expect(404)
+            .end(done)
+    });
+    it('Should return 404 for non-object ids', (done) => {
+        request(app)
+            .get('/todos/123')
+            .expect(404)
+            .end(done);
+    });
+})
