4b92924d0dbf79a9eb70c8d34b05f91395404640
Pattarawan Sriprasert <pattarawan@Macintosh.local>
2017-07-23 22:12:45 +0700

    Setting up ENV

---
+++ config/config.js
@@ -0,0 +1,11 @@
+var env = process.env.NODE_ENV || 'development';
+console.log('env *****', env);
+if (env === 'development') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}else {
+    process.env.MONGODB_URI =  'mongodb://admin:noblecooleyy@ds119223.mlab.com:19223/todoapp');
+}
