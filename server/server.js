87325d27db8ececb87d89f972c364e8f74af5543
Pattarawan Sriprasert <pattarawan@Macintosh.local>
2017-07-23 16:53:16 +0700

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
+const _ = require('lodash');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -57,9 +58,37 @@ app.delete('/todos/:id', (req, res) => {
     if (!todo) {
       return res.status(404).send({});
     }
-    res.send({todo});
+    res.send({ todo });
   })
 });
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed'])
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then((todo) => {
+      if (!todo) {
+        return res.send(404).send();
+      }
+      res.send({ todo });
+    }).catch((e) => {
+      res.send(404);
+    });
+
+});
+
 
 app.listen(port, () => {
   console.log(`Started on port : ${port}`);
