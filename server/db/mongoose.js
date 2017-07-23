43211c2a589d11a022e41869d63a9417b82653eb
Pattarawan Sriprasert <pattarawan@Macintosh.local>
2017-07-23 16:54:57 +0700

    Add PATCH /todos/:id on heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://admin:noblecooleyy@ds119223.mlab.com:19223/todoapp');
 
 module.exports = {
     mongoose
