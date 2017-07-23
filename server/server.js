ea96458bb1224a50a56b5dbb9892855f342e4c56
Pattarawan Sriprasert <pattarawan@Macintosh.local>
2017-07-23 14:49:49 +0700

    Modified server.js for add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -29,10 +30,25 @@ app.get('/todos', (req, res) => {
   })
 });
 
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send({});
+    }
+    res.send({ todo });
+  }).catch((e) => {
+    res.status(404).send();
+  });
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
-})
+});
 
 module.exports = {
   app
