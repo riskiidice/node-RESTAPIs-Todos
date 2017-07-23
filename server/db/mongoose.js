521acfff0db21852fc7a5591e6cb4ad3db4a7915
Pattarawan Sriprasert <pattarawan@Macintosh.local>
2017-07-23 15:37:07 +0700

    Set up for heroku and using mLab Mongodb

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://ds119223.mlab.com:19223/todoapp -u admin -p noblecooleyy');
 
 module.exports = {
     mongoose
